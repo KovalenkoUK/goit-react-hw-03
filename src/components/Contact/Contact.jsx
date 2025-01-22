49d7526e995fd1884373d79dc6c346854632a4d5
Artem Garkushka <Artem.Garkushka@gmail.com>
2025-01-22 18:52:20 +0200

    add

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import App from '../../App';
-import ContactList from '../ContactList/ContactList'
+import ContactList from '../ContactList/ContactList';
 import './Contact.module.css';
 
 const Contact = ({ contact, onDeleteContact }) => {
   return (
     <li>
       <p>{contact.name}: {contact.number}</p>
-      <button onClick={() => onDeleteContact(contact.id)}>Delete</button>
+      <button onClick={() => onDeleteContact(contact.id)} className="delete-button">Delete</button>
     </li>
   );
 };
