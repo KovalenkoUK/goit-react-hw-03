f0d82f047cdbbc394c9d298f7e52219bce41df9d
Artem Garkushka <Artem.Garkushka@gmail.com>
2025-01-22 11:39:59 +0200

    add

--- src/App.jsx
+++ src/App.jsx
@@ -9,19 +9,28 @@ import './App.module.css';
 
 const App = () => {
 
-    const [contacts, setContacts] = useState([
+    const [contacts] = useState([
         {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
         {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
         {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
         {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
     ]);
     
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const handleSearchChange =(e) => {
+        setSearchTerm(e.target.value);
+    };
+
+    const filteredContacts = contacts.filter(contact =>
+        contact.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
 
     return (
     <div>
     <h1>Phonebook</h1>
     <ContactForm />
-    <SearchBox />
+    <SearchBox value={searchTerm} onChange={handleSearchChange}/>
     <ContactList contacts = {contacts} />
     </div>
     )
--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import './SearchBox.module.css';
 
-
+const SearchBox = ({value, onChange}) =>{
+    return (
+        <input type="text" placeholder="Search contacts..." value={value} onChange={onChange}/>
+    );
+};
 
 
 
