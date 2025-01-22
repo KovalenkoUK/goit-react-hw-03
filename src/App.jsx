88d5a0679264946e5b41ae126b2021acf3b87455
Artem Garkushka <Artem.Garkushka@gmail.com>
2025-01-22 11:07:50 +0200

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Formik } from 'formik';
 import Contact from './components/Contact/Contact';
 import ContactForm from './components/ContactForm/ContactForm';
@@ -7,6 +7,12 @@ import SearchBox from './components/SearchBox/SearchBox';
 
 
 const App = () => {
+    const [contacts, setContacts] = useState([
+        {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+        {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+        {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+        {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+    ])
     
 
     return (
@@ -14,7 +20,9 @@ const App = () => {
     <h1>Phonebook</h1>
     <ContactForm />
     <SearchBox />
-    <ContactList />
+    <ContactList contacts={contacts} />
     </div>
     )
 }
+
+export default App;
